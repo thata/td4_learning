9a2eea59cb5f5b48c1cbddc37ade9549e352166d
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-07-25 04:52:44 +0900

    Add rom

---
+++ rom_test.v
@@ -0,0 +1,17 @@
+module rom_test();
+  reg [3:0] adr;
+  wire [7:0] dout;
+
+  rom rom0 (adr, dout);
+
+  initial begin
+    $monitor("%t: adr = %b, dout = %b", $time, adr, dout);
+
+    // ROM がそれっぽく動くことを確認
+    // ROM の中身は任意に変更可能なので、値の検証は行わない。
+    adr = 4'b0000; #10;
+    adr = 4'b0001; #10;
+    adr = 4'b0010; #10;
+    adr = 4'b1111; #10;
+  end
+endmodule
