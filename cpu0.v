d11175c3390e37697a8369b02cab794ab85e6de7
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-07-25 13:14:48 +0900

    fix comment

--- cpu0.v
+++ cpu0.v
@@ -1,4 +1,4 @@
-# 1bit flip flop
+// 1bit flip flop
 module flipflop(
   input clk, reset, d,
   output reg q);
@@ -10,7 +10,7 @@ module flipflop(
       q <= d;
 endmodule
 
-# 1bit CPU
+// 1bit CPU
 module cpu0(
   input clk, reset,
   output q);
